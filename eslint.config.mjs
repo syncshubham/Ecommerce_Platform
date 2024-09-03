53778543014d0e72f5ab410efe6481f8b432149d
syncshubham <syncshubham@gmail.com>
2024-09-03 17:06:36 +0530

    add prettier rc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,14 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+export default [{
+    files: ["**/*.{js,mjs,cjs,ts}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
