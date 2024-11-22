0b26af663a5942338f935d2524e662f55dddf77e
syncshubham <syncshubham@gmail.com>
2024-11-22 15:21:56 +0530

    tested by changing the ports

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import app from './src/app';
 import { config } from './src/config/config';
 
 const startServer = () => {
-    const port = config.port || 3000;
+    const port = config.port || 5000;
 
     app.listen(port, () => {
         console.log(`listening on port ${port}`);
